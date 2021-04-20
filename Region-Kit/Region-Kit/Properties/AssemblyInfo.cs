8bdf3addd7f59201242f66b004c040dbade4ea93
DeltatimeA <70550216+DeltatimeA@users.noreply.github.com>
2021-04-20 07:34:51 -0400

    changed version numbers

--- Region-Kit/Region-Kit/Properties/AssemblyInfo.cs
+++ Region-Kit/Region-Kit/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Region-Kit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DryCryCrystal & DeltaTime")]
 [assembly: AssemblyProduct("Region-Kit")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion(RegionKit.RegionKit.modVersion)]

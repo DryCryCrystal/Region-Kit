47e39c34861b2e024d9ead124fa3676332dc7e3d
Thrithralas <48440160+Thrithralas@users.noreply.github.com>
2021-08-25 20:16:48 +0200

    Fixed SuperstructureFusesFix, Added Vector2ArrayField and Drawable

---
+++ Region-Kit/Region-Kit/ExtraPOM/Vector2ArrayField.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevInterface;
+using RWCustom;
+using UnityEngine;
+
+public static partial class PlacedObjectsManager {
+    /// <summary>
+    /// A <see cref="ManagedField"/> that stores an array of <see cref="Vector2"/>s
+    /// </summary>
+    public class Vector2ArrayField : ManagedField {
+        public Vector2ArrayRepresentationType RepresentationType;
+        public int NodeCount;
+        public bool IncludeParent;
+        
+        /// <summary>
+        /// Creates a <see cref="Vector2ArrayField"/> and assigns the proper values that are used in the handle
+        /// </summary>
+        /// <param name="key">The key of the field that should be used in <see cref="ManagedData.GetValue{T}"/></param>
+        /// <param name="nodeCount">The number of <see cref="Vector2"/>s that the field stores</param>
+        /// <param name="includeParent">Sets if the field should use the parent as the first node</param>
+        /// <param name="representationType">The type of the representation that should be created.</param>
+        /// <param name="nodes">Default nodes that are assigned. Node ordering is bottom nodes left to right, then top nodes right to left.</param>
+        public Vector2ArrayField(string key, int nodeCount, bool includeParent, Vector2ArrayRepresentationType representationType = Vector2ArrayRepresentationType.Polygon, params Vector2[] nodes) : base(key, ProcessNodes(nodes, nodeCount)) {
+            NodeCount = nodeCount;
+            RepresentationType = representationType;
+            IncludeParent = includeParent;
+        }
+
+        public override string ToString(object value) {
+            Vector2[] vectors = (Vector2[])value;
+            return string.Join("^", vectors.Select(v => $"{v.x};{v.y}").ToArray());
+        }
+
+        public override object FromString(string str) {
+            List<Vector2> positions = new List<Vector2>();
+            foreach (string substring in str.Split('^')) {
+                string[] split = substring.Split(';');
+                positions.Add(new Vector2(float.Parse(split[0]), float.Parse(split[1])));
+            }
+
+            return positions.ToArray();
+        }
+
+        private static object ProcessNodes(Vector2[] vector2s, int nodeCount) {
+            if (vector2s is null) return new Vector2[nodeCount];
+            Vector2[] result = new Vector2[nodeCount];
+            Array.Copy(vector2s, result, vector2s.Length < nodeCount ? vector2s.Length : nodeCount);
+            return result;
+        }
+
+        public override DevUINode MakeAditionalNodes(ManagedData managedData, ManagedRepresentation managedRepresentation) {
+            return new Vector2ArrayHandle(this, managedData, managedRepresentation);
+        }
+
+        public enum Vector2ArrayRepresentationType {
+            Chain,
+            Polygon
+        }
+
+        public class Vector2ArrayHandle : PositionedDevUINode {
+            public Vector2ArrayField Field;
+            public ManagedData Data;
+            public PositionedDevUINode First;
+            public List<int> Lines = new List<int>();
+
+            public Vector2ArrayHandle(Vector2ArrayField field, ManagedData data, ManagedRepresentation representation) : base(representation.owner, field.key, representation, data.GetValue<Vector2[]>(field.key)[0]) {
+                Field = field;
+                Data = data;
+                bool includeParent = Field.IncludeParent;
+                if (includeParent) {
+                    First = parentNode as PositionedDevUINode;
+                }
+                else {
+                    First = new Handle(owner, field.key + "_0", this, Data.GetValue<Vector2[]>(field.key)[0]);
+                    subNodes.Add(First);
+                }
+
+                for (int i = 1; i < field.NodeCount; i++) {
+                    int currentLine = fSprites.Count;
+                    PositionedDevUINode next = new Handle(owner, field.key + "_" + i, this, Data.GetValue<Vector2[]>(field.key)[i]);
+                    subNodes.Add(next);
+                    fSprites.Add(new FSprite("pixel"));
+                    owner.placedObjectsContainer.AddChild(fSprites[currentLine]);
+                    fSprites[currentLine].anchorY = 0;
+                    Lines.Add(currentLine);
+                }
+
+                if (Field.RepresentationType == Vector2ArrayRepresentationType.Polygon) {
+                    int currLine = fSprites.Count;
+                    fSprites.Add(new FSprite("pixel"));
+                    owner.placedObjectsContainer.AddChild(fSprites[currLine]);
+                    fSprites[currLine].anchorY = 0;
+                    Lines.Add(currLine);
+                }
+            }
+
+            public override void Move(Vector2 newPos) {
+                First.Move(newPos);
+                Vector2[] vArr = Data.GetValue<Vector2[]>(Field.key);
+                vArr[0] = Field.IncludeParent ? new Vector2(0, 0) : newPos;
+                Data.SetValue(Field.key, vArr);
+                base.Move(newPos);
+            }
+
+            public override void Refresh() {
+                base.Refresh();
+                PositionedDevUINode start = First;
+                int offset = Field.IncludeParent ? 0 : 1;
+                for (int i = offset; i < subNodes.Count; i++) {
+                    if (!(subNodes[i] is PositionedDevUINode end)) {
+                        throw new NullReferenceException("end node cannot be null!!");
+                    }
+
+                    int lineIndex = Lines[i - offset];
+                    MoveSprite(lineIndex, start.absPos);
+                    FSprite lineSprite = fSprites[lineIndex];
+                    lineSprite.scaleY = (end.absPos - start.absPos).magnitude;
+                    lineSprite.rotation = Custom.VecToDeg(end.absPos - start.absPos);
+                    start = end;
+                }
+
+                if (Field.RepresentationType == Vector2ArrayRepresentationType.Polygon) {
+                    int lastIndex = Lines[Lines.Count - 1];
+                    MoveSprite(lastIndex, start.absPos);
+                    FSprite lineSprite = fSprites[lastIndex];
+                    lineSprite.scaleY = (start.absPos - First.absPos).magnitude;
+                    lineSprite.rotation = Custom.VecToDeg(First.absPos - start.absPos);
+                }
+            }
+        }
+
+        public static void OnPositionedDevUINodeMove(On.DevInterface.PositionedDevUINode.orig_Move orig, PositionedDevUINode self, Vector2 newpos) {
+            orig(self, newpos);
+            if (self.parentNode is Vector2ArrayHandle v2ah) {
+                Vector2[] vectors = v2ah.Data.GetValue<Vector2[]>(v2ah.Field.key);
+                int index = int.Parse(self.IDstring.Split('_')[1]);
+                if (index == 0) return;
+                vectors[index] = newpos;
+                v2ah.Data.SetValue(v2ah.Field.key, vectors);
+            }
+        }
+    }
+}
---
+++ Region-Kit/Region-Kit/Objects/Drawable.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using RWCustom;
+using UnityEngine;
+
+namespace RegionKit.Objects {
+    public class Drawable : CosmeticSprite {
+        public static PlacedObjectsManager.ManagedField[] Fields = {
+            new PlacedObjectsManager.Vector2ArrayField("quad", 4, true, PlacedObjectsManager.Vector2ArrayField.Vector2ArrayRepresentationType.Polygon, Vector2.zero, Vector2.right * 20f, (Vector2.right + Vector2.up) * 20f, Vector2.up * 20f),
+            new PlacedObjectsManager.StringField("spriteName", "Futile_White", "Decal Name"),
+            new PlacedObjectsManager.FloatField("depth", 0f, 1f, 1f, displayName: "Depth"),
+            new PlacedObjectsManager.StringField("shader", "Basic", "Shader"),
+            new PlacedObjectsManager.EnumField("container", typeof(FContainer), FContainer.Foreground, displayName: "FContainer"),
+            new PlacedObjectsManager.IntegerField("alpha", 1, 255, 255, PlacedObjectsManager.ManagedFieldWithPanel.ControlType.slider, "Alpha"),
+            new PlacedObjectsManager.BooleanField("useColour", false, displayName: "Use Colour"),
+            new PlacedObjectsManager.ColorField("colour", Color.white, PlacedObjectsManager.ManagedFieldWithPanel.ControlType.slider, "Colour")
+        };
+
+        public Drawable(PlacedObject pObj, Room room) {
+            this.room = room;
+            LocalPlacedObject = pObj;
+        }
+
+        public enum FContainer {
+            Shadows,
+            BackgroundShortcuts,
+            Background,
+            Midground,
+            Items,
+            Foreground,
+            ForegroundLights,
+            Shortcuts,
+            Water,
+            GrabShaders,
+            Bloom,
+            HUD,
+            HUD2
+        }
+
+        public PlacedObjectsManager.ManagedData Data => LocalPlacedObject.data as PlacedObjectsManager.ManagedData;
+
+        public Vector2 PlacedObjectTile => LocalPlacedObject.pos;
+
+
+        public PlacedObject LocalPlacedObject { get; }
+
+        public override void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam) {
+            base.InitiateSprites(sLeaser, rCam);
+            TriangleMesh.Triangle[] triangles = new TriangleMesh.Triangle[2];
+            triangles[0] = new TriangleMesh.Triangle(0, 1, 2);
+            triangles[1] = new TriangleMesh.Triangle(1, 2, 3);
+            TriangleMesh mesh = new TriangleMesh("Futile_White", triangles, true) {
+                UVvertices = {
+                    [0] = new Vector2(0, 0),
+                    [1] = new Vector2(1, 0),
+                    [2] = new Vector2(0, 1),
+                    [3] = new Vector2(1, 1)
+                }
+            };
+            sLeaser.sprites = new FSprite[] { mesh };
+        }
+
+        public Vector2[] Quad => new[] { Data.GetValue<Vector2[]>("quad")[0], Data.GetValue<Vector2[]>("quad")[1], Data.GetValue<Vector2[]>("quad")[3], Data.GetValue<Vector2[]>("quad")[2] };
+
+        public override void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos) {
+            base.DrawSprites(sLeaser, rCam, timeStacker, camPos);
+            sLeaser.sprites[0].alpha = Data.GetValue<int>("alpha") / 255f;
+            rCam.ReturnFContainer(Data.GetValue<FContainer>("container").ToString())
+                .AddChildAtIndex(sLeaser.sprites[0],
+                    Mathf.FloorToInt(
+                        Data.GetValue<float>("depth") *
+                        rCam.ReturnFContainer(Data.GetValue<FContainer>("container").ToString())
+                            .GetChildCount()));
+            try {
+                sLeaser.sprites[0].SetElementByName(Data.GetValue<string>("spriteName"));
+            }
+            catch (FutileException) {
+                try {
+                    WWW www = new WWW(string.Concat("file:///", Custom.RootFolderDirectory(), "Assets", Path.DirectorySeparatorChar, "Futile", Path.DirectorySeparatorChar, "Resources", Path.DirectorySeparatorChar, "Decals", Path.DirectorySeparatorChar, Data.GetValue<string>("spriteName"), ".png"));
+                    Texture2D tex = new Texture2D(1, 1, TextureFormat.ARGB32, false) {
+                        wrapMode = TextureWrapMode.Clamp,
+                        anisoLevel = 0,
+                        filterMode = FilterMode.Point
+                    };
+                    www.LoadImageIntoTexture(tex);
+                    HeavyTexturesCache.LoadAndCacheAtlasFromTexture(Data.GetValue<string>("spriteName"), tex);
+                    sLeaser.sprites[0].SetElementByName(Data.GetValue<string>("spriteName"));
+                }
+                catch (Exception e) when (e is FutileException) {
+                    //ignored
+                }
+                catch (Exception e) when (e is IOException) {
+                    //ignored
+                }
+            }
+
+            for (int i = 0; i < 4; i++) {
+                ((TriangleMesh)sLeaser.sprites[0]).MoveVertice(i, PlacedObjectTile + Quad[i] - camPos);
+            }
+
+            if (rCam.game.rainWorld.Shaders.ContainsKey(Data.GetValue<string>("shader"))) {
+                sLeaser.sprites[0].shader = rCam.game.rainWorld.Shaders[Data.GetValue<string>("shader")];
+            }
+
+            var col = Data.GetValue<Color>("colour");
+            col.a = Data.GetValue<int>("alpha") / 255f;
+            sLeaser.sprites[0].color = Data.GetValue<bool>("useColour") ? col : new Color(Color.white.r, Color.white.g, Color.white.b, Data.GetValue<int>("alpha") / 255f);
+        }
+
+        public static void Register() => PlacedObjectsManager.RegisterFullyManagedObjectType(Fields, typeof(Drawable), "FreeformDecalOrSprite");
+    }
+}

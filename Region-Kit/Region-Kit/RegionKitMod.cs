fe4f3e7a13775c57223b8159c3d51467d42589cb
DryCryCrystal <ryan2401@hotmail.co.uk>
2022-02-13 17:28:14 +0000

    Update RegionKitMod.cs

--- Region-Kit/Region-Kit/RegionKitMod.cs
+++ Region-Kit/Region-Kit/RegionKitMod.cs
@@ -12,7 +12,7 @@ using System.Linq;
 namespace RegionKit {
     public class RegionKitMod : PartialityMod {
 
-        public const string modVersion = "1.2.1";
+        public const string modVersion = "1.3";
         public const string buildVersion = "1"; //Increments for every code change without a version change.
 
         public RegionKitMod() {
@@ -357,7 +357,7 @@ namespace RegionKit {
     // This URL is specific to this mod, and identifies it on AUDB.
     public string updateURL = "http://beestuff.pythonanywhere.com/audb/api/mods/7/0";
         // Version - increase this by 1 when you upload a new version of the mod.
-        public int version = 11;
+        public int version = 12;
         // Public key in base64 - don't touch!
         public string keyE = "AQAB";
         public string keyN = "pQTSWONMkz/+cDljDGQPVe33mzBTAjabsB8++ZF7h+5rx65KSpvqviESF8X6tKFZPQBxaQD+JwLK05kSt9lopcUsLe8T+Vxia4HXDnEGmAMuZg477vpib+JCgKP0pAMjwtLiD8GpvbI3kUcxD8qJ3+l6ULCTbT8Z120U2lae22AzMU5Tpz0Yvl/vATv3472roBYe7N9LA5mFaACPT+E+U36/hSoIhtVmIxtbOXCmCod/k4L3/CPDs4w34gb1Vo43GiLLo9jOSXVPhMTMkWHrYWnEWy4tu9Ujcj0KZcuHGylO6MYfV+dSJwdAgkcFuq4plNRHt+pmAnwbI0U5kcd2FlpkI2ihqVShvDyj4v3mFNmd/0YighTcBXmYQj3h06NKup9cPfNCPRdwP9CTNjtLljA+SWkl7z/j+z29lWFuE6a1xNiYZb+GGj4UbExUDgcZ1YFOqSgPeQPeoFGqY5nGBQN0UOv/9GmrdaxxWGDrgkbRL2+L/NwKV2uH8HVBzSu0VBRnTjz3JTzkKTBR+ai0LDfmez7BBvG8giTvgNrHi3LxxvVGUugj9GnbRxnTSY6By8JKSwKkgPztVb+irUPW+1lQv76Gyx6fh/8V/+EbrgKSVUEH/mF/Yg8MDQseRbF7X697ZNcfiHm/dGjV+zNcR8CL0Tvtj2mqdNPH4Eib/qE=";

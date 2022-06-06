0c027affd3ac8a0340c47be93f41c94c61aaa9dd
Delbatime <70550216+Delbatime@users.noreply.github.com>
2022-06-05 20:22:00 -0400

    Update ARKillRect.cs

--- Region-Kit/Region-Kit/ARKillRect.cs
+++ Region-Kit/Region-Kit/ARKillRect.cs
@@ -35,8 +35,7 @@ namespace RegionKit
                 {
                     for (int k = 0; k < room.physicalObjects[i][j].bodyChunks.Length; k++)
                     {
-                        Vector2 a = room.physicalObjects[i][j].bodyChunks[k].ContactPoint.ToVector2();
-                        Vector2 v = room.physicalObjects[i][j].bodyChunks[k].pos + a * (room.physicalObjects[i][j].bodyChunks[k].rad + 30f);
+                        Vector2 v = room.physicalObjects[i][j].bodyChunks[k].pos * (room.physicalObjects[i][j].bodyChunks[k].rad + 30f);
                         if (Custom.InsideRect(room.GetTilePosition(v), rect))
                         {
                             if (room.physicalObjects[i][j] is Creature)
